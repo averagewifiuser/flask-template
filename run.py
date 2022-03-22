a9ef3b00f93f69ec98598216b43684994eb02d5e
@joseph3241 <joseph3241@gmail.com>
2022-03-22 15:13:23 +0000

    slight changes

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
 from app import create_app
-from flask_script import Server, Manager, Shell
 
-def _make_context():
-    return dict(app=create_app)
+app = create_app()
 
-
-manager = Manager(create_app)
-manager.add_command("runserver", Server())
-manager.add_command("shell", Shell(make_context=_make_context))
-
-if __name__ == "__main__":
-    manager.run()
+if __name__ == '__main__':
+    app.run()
